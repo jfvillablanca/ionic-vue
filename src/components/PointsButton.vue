35ccac005bd3b3627ab7796d95519fa39a9141bf
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-09-29 06:59:09 +0800

    style: PointsButton

--- src/components/PointsButton.vue
+++ src/components/PointsButton.vue
@@ -1,14 +1,35 @@
 <template>
-  <ion-button :router-link="`/loyalty`">
-    <!-- add crown icon -->
+  <ion-button
+    fill="clear"
+    :router-link="`/loyalty`"
+  >
+    <span>👑</span>
     <strong>{{ `${profile?.points ?? 0} Points` }}</strong>
-    <!-- add right arrow icon -->
+    <ion-icon :icon="chevronForwardOutline" />
   </ion-button>
 </template>
 
 <script setup lang="ts">
-import { IonButton } from '@ionic/vue';
+import { IonButton, IonIcon } from '@ionic/vue';
 import { ProfileKey } from '@/symbols';
 import { inject } from 'vue';
+import { chevronForwardOutline } from 'ionicons/icons';
+
 const profile = inject(ProfileKey);
 </script>
+
+<style scoped>
+strong {
+  color: var(--ion-color-light-contrast);
+}
+
+span {
+  font-size: 2rem;
+  margin-right: 0.7rem;
+}
+
+strong {
+  text-transform: capitalize;
+  margin-inline: 1rem;
+}
+</style>
