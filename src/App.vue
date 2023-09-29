1e835e344eb26f8b90da5da93c6e4e087dc05e3b
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-09-29 12:41:53 +0800

    style: ~match design spec for sidebar menu

--- src/App.vue
+++ src/App.vue
@@ -6,23 +6,28 @@
         type="overlay"
       >
         <ion-content>
-          <ion-list id="inbox-list">
+          <ion-list>
             <!-- Profile Header -->
-            <div>
-              <div class="header-buttons">
-                <ion-avatar>
-                  <img
-                    :alt="profile?.firstName"
-                    :src="profile?.image"
-                  >
-                </ion-avatar>
-                <ion-menu-toggle>
-                  <points-button :profile="profile" />
-                </ion-menu-toggle>
+            <ion-item
+              lines="none"
+              :style="{ '--padding-bottom': '1.7rem' }"
+            >
+              <div>
+                <div class="sidebar-header-buttons">
+                  <ion-avatar>
+                    <img
+                      :alt="profile?.firstName"
+                      :src="profile?.image"
+                    >
+                  </ion-avatar>
+                  <ion-menu-toggle>
+                    <points-button :profile="profile" />
+                  </ion-menu-toggle>
+                </div>
+                <ion-header>{{ fullName }}</ion-header>
+                <ion-note>{{ profile.phone }}</ion-note>
               </div>
-              <ion-header>{{ fullName }}</ion-header>
-              <ion-note>{{ profile.phone }}</ion-note>
-            </div>
+            </ion-item>
 
             <ion-menu-toggle
               :auto-hide="false"
@@ -33,20 +38,43 @@
                 @click="selectedIndex = i"
                 router-direction="root"
                 :router-link="p.url"
-                lines="none"
+                :lines="i !== 4 ? 'none' : 'full'"
+                :style="
+                  i === 4
+                    ? { '--padding-bottom': '1.7rem' }
+                    : i === 5
+                      ? { '--padding-top': '1.7rem' }
+                      : ''
+                "
                 :detail="false"
                 class="hydrated"
-                :class="{ selected: selectedIndex === i }"
               >
                 <ion-icon
                   aria-hidden="true"
                   slot="start"
-                  :ios="p.iosIcon"
-                  :md="p.mdIcon"
+                  size="small"
+                  :color="i === 0 ? 'primary' : 'medium'"
+                  :icon="p.icon"
                 />
                 <ion-label>{{ p.title }}</ion-label>
+                <ion-note
+                  v-if="p.counter"
+                  slot="end"
+                  class="counter"
+                >
+                  {{
+                    p.counter
+                  }}
+                </ion-note>
               </ion-item>
             </ion-menu-toggle>
+
+            <ion-item
+              lines="none"
+              class="logout"
+            >
+              <ion-button>Logout</ion-button>
+            </ion-item>
           </ion-list>
         </ion-content>
       </ion-menu>
@@ -78,16 +106,16 @@ import {
 } from '@ionic/vue';
 import { provide, ref } from 'vue';
 import {
-  archiveOutline,
-  archiveSharp,
-  heartOutline,
+  bagHandleSharp,
+  fastFoodSharp,
   heartSharp,
-  mailOutline,
-  mailSharp,
-  paperPlaneOutline,
-  paperPlaneSharp,
-  trashOutline,
-  trashSharp,
+  helpCircleSharp,
+  homeSharp,
+  locationSharp,
+  notificationsSharp,
+  personSharp,
+  storefrontSharp,
+  timeSharp,
 } from 'ionicons/icons';
 
 const selectedIndex = ref(0);
@@ -95,32 +123,54 @@ const appPages = [
   {
     title: 'Home',
     url: '/home',
-    iosIcon: mailOutline,
-    mdIcon: mailSharp,
+    icon: homeSharp,
   },
   {
     title: 'Order Now!',
     url: '/menu',
-    iosIcon: paperPlaneOutline,
-    mdIcon: paperPlaneSharp,
+    icon: fastFoodSharp,
   },
   {
     title: 'Notifications',
     url: '/home',
-    iosIcon: heartOutline,
-    mdIcon: heartSharp,
+    icon: notificationsSharp,
+    counter: 4,
   },
   {
     title: 'Store Locator',
     url: '/home',
-    iosIcon: archiveOutline,
-    mdIcon: archiveSharp,
+    icon: storefrontSharp,
   },
   {
     title: "FAQ's",
     url: '/home',
-    iosIcon: trashOutline,
-    mdIcon: trashSharp,
+    icon: helpCircleSharp,
+  },
+  {
+    title: 'My Orders',
+    url: '/home',
+    icon: bagHandleSharp,
+    counter: 2,
+  },
+  {
+    title: 'My Account',
+    url: '/home',
+    icon: personSharp,
+  },
+  {
+    title: 'My Favorites',
+    url: '/home',
+    icon: heartSharp,
+  },
+  {
+    title: 'Order Tracker',
+    url: '/home',
+    icon: locationSharp,
+  },
+  {
+    title: 'Order History',
+    url: '/home',
+    icon: timeSharp,
   },
 ];
 
@@ -138,16 +188,54 @@ if (path !== undefined) {
 </script>
 
 <style scoped>
-.header-buttons {
+.sidebar-header-buttons {
   display: flex;
-  align-items: end;
+  align-items: center;
 }
 
-.header-buttons ion-avatar {
-  margin: 1rem;
+.sidebar-header-buttons ion-avatar {
+  margin-right: 1rem;
+  margin-block: 1rem;
 }
 
-.header-buttons ion-menu-toggle {
+.sidebar-header-buttons ion-menu-toggle {
   flex: 1;
+  display: grid;
+  place-items: center;
+}
+
+ion-header {
+  font-size: 2.3rem;
+  font-weight: 900;
+}
+
+ion-note {
+  font-size: 1.4rem;
+}
+
+ion-list {
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+}
+
+.logout {
+  flex: 1;
+  align-items: end;
+  --min-height: 70%;
+}
+
+.logout ion-button::part(native) {
+  --border-radius: 1.6rem;
+  padding: 1.7rem 5rem;
+  font-weight: bold;
+}
+
+.counter {
+  background-color: var(--ion-color-secondary);
+  color: var(--ion-color-secondary-contrast);
+  font-size: 1rem;
+  padding: 0.4rem 0.7rem;
+  border-radius: 100%;
 }
 </style>
