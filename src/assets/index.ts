f6c47ed66997d872f3526df4dfe600660ff04a82
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-09-29 15:50:01 +0800

    add assets and add d.ts for asset file types

---
+++ src/assets/index.ts
@@ -0,0 +1,52 @@
+import '../../global.d.ts';
+
+import logoSvg from './logo.svg';
+
+import chickenSalad from './food/chicken-salad.png';
+import friedChicken from './food/fried-chicken.png';
+import sorvetesPrimavera from './food/sorvetes-primavera.png';
+import steakFriesVeggies from './food/steak-fries-veggies.png';
+
+import meat from './categories/meat.png';
+import dessert from './categories/dessert.png';
+
+import avatar from './general/avatar.png';
+import crown from './general/crown.svg';
+import thumbsUp from './general/thumbs-up.png';
+import menu from './general/menu.svg';
+import notif from './general/notif.svg';
+
+import paypal from './payment/paypal.svg';
+import paynamics from './payment/paypal.svg';
+
+import whatsNew from './promotions/whats-new-1.png';
+import specialOffer1 from './promotions/special-offers-1.png';
+import specialOffer2 from './promotions/special-offers-2.png';
+
+export const logo = logoSvg;
+export const food = {
+  chickenSalad,
+  friedChicken,
+  sorvetesPrimavera,
+  steakFriesVeggies,
+};
+export const category = {
+  dessert,
+  meat,
+};
+export const general = {
+  avatar,
+  crown,
+  menu,
+  notif,
+  thumbsUp,
+};
+export const payment = {
+  paynamics,
+  paypal,
+};
+export const promotions = {
+  specialOffer1,
+  specialOffer2,
+  whatsNew,
+};
