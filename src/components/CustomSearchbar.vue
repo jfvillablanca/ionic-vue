4e02dc4700aea84dc8a10d7081f38b5fabbb0b4f
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-09-29 18:13:36 +0800

    add CustomSearchbar component

---
+++ src/components/CustomSearchbar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="searchbar">
+    <ion-searchbar
+      class="override"
+      :placeholder="`try our new ${
+        !!menuItems ? menuItems[0].name : 'delights'
+      }`"
+    />
+    <ion-button
+      v-if="enableFilter"
+      fill="clear"
+    >
+      <ion-icon :icon="options" />
+    </ion-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { IonButton, IonIcon, IonSearchbar } from '@ionic/vue';
+import { MenuItemArrayKey } from '@/symbols';
+import { options } from 'ionicons/icons';
+import { inject } from 'vue';
+
+const menuItems = inject(MenuItemArrayKey);
+
+const { enableFilter } = defineProps<{ enableFilter?: boolean }>();
+</script>
+
+<style scoped>
+ion-searchbar {
+  --icon-color: var(--ion-color-primary);
+  --clear-button-color: var(--ion-color-medium-shade);
+}
+
+.searchbar {
+  display: flex;
+
+  margin-block: 2rem;
+
+  border-radius: 1.6rem;
+  box-shadow: inset 0 0 1px 1px var(--ion-color-medium-tint);
+  background-color: var(--ion-color-light-shade);
+}
+
+.searchbar ion-searchbar.override {
+  --background: transparent;
+
+  flex: 1;
+}
+
+.searchbar svg {
+  stroke-width: 2px;
+}
+</style>
--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { default as PointsButton } from './PointsButton.vue';
 export { default as SectionHeader } from './SectionHeader.vue';
 export { default as MenuCardItem } from './MenuCardItem.vue';
 export { default as AddToCartModal } from './AddToCartModal.vue';
+export { default as CustomSearchbar } from './CustomSearchbar.vue';
