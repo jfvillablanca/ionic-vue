5ce84defdc4c75d558f118ce683c469da9ba27f9
jfvillablanca <31008330+jfvillablanca@users.noreply.github.com>
2023-10-01 06:48:35 +0800

    style: ~match checkout modal to mockup

--- src/components/SuccessCheckoutModal.vue
+++ src/components/SuccessCheckoutModal.vue
@@ -1,23 +1,32 @@
 <template>
-  <ion-content>
-    <div>
-      <!-- insert thumbs up clip art -->
-    </div>
-    <h1>Order Received</h1>
+  <div class="ion-padding">
+    <img
+      :src="thumbsUp"
+      alt="thumbs up"
+    >
+    <h1>Order Received!</h1>
     <h2>
-      Your order No. {{ placeholderOrderNumber }} is successful and is now being
-      processed. Kindly wait for our notification regarding your order.
+      Your order <span>No. {{ placeholderOrderNumber }}</span> is successful and
+      is now being processed. Kindly wait for our notification regarding your
+      order.
     </h2>
-    <ion-button> Track Order </ion-button>
-    <ion-button @click="goHome">
-      Back to Home
-    </ion-button>
-  </ion-content>
+    <div class="buttons">
+      <ion-button> Track Order </ion-button>
+      <ion-button
+        fill="clear"
+        @click="goHome"
+      >
+        Back to Home
+      </ion-button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts" setup>
-import { IonContent, IonButton, modalController } from '@ionic/vue';
+import { IonButton, modalController } from '@ionic/vue';
 import { useRouter } from 'vue-router';
+import { general } from '@/assets';
+const { thumbsUp } = general;
 const router = useRouter();
 
 const placeholderOrderNumber = '12343456';
@@ -28,4 +37,60 @@ function goHome() {
 }
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+div {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  font-weight: bold;
+
+  height: 100%;
+  padding-top: 15rem;
+
+  * {
+    font-size: 1.6rem;
+    font-weight: 500;
+  }
+
+  img {
+    height: 20rem;
+    margin-bottom: 2rem;
+  }
+
+  h1 {
+    font-size: 2.8rem;
+    font-weight: 700;
+  }
+
+  h2 {
+    align-self: center;
+    text-align: center;
+
+    margin-top: 0.5rem;
+    margin-bottom: 2rem;
+    font-weight: 400;
+    color: var(--ion-color-medium-shade);
+
+    span {
+      color: var(--ion-color-primary);
+      font-weight: 600;
+    }
+  }
+
+  .buttons {
+    width: 100%;
+    justify-self: end;
+
+    ion-button {
+      width: 100%;
+      font-weight: 700;
+    }
+
+    ion-button::part(native) {
+      padding-block: 2.2rem;
+    }
+  }
+}
+</style>
